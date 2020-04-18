fe8a90fb84b75c0f9ae1e3bcc877fb9b5a7638f5
JosephVC <josephvcardenas@gmail.com>
2020-04-17 18:05:48 -0700

    added superuser and made a class name more conventional

--- ocr_backend/ocr/admin.py
+++ ocr_backend/ocr/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import OCR_file
+from .models import OCRFile
 
 # Register your models here.
-admin.site.register.(OCR_file)
+admin.site.register(OCRFile)
--- ocr_backend/ocr/models.py
+++ ocr_backend/ocr/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class OCR_file(models.Model):
+class OCRFile(models.Model):
     title = models.CharField(max_length=200)
     description =  models.TextField()
 
