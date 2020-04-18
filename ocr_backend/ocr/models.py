0c46ab54c404a64d104877fdb42b0811a768a614
JosephVC <josephvcardenas@gmail.com>
2020-04-17 18:10:12 -0700

    updated migrations

--- ocr_backend/ocr/models.py
+++ ocr_backend/ocr/models.py
@@ -5,6 +5,8 @@ class OCRFile(models.Model):
     title = models.CharField(max_length=200)
     description =  models.TextField()
 
+    # TODO: create a form to upload a file
+
     def __str__(self):
         """ A string representation of the model. """
         return self.title 
