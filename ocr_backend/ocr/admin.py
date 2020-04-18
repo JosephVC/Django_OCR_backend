867d6ae3ee2fe08cbdc615451cad7dc4053fac65
JosephVC <josephvcardenas@gmail.com>
2020-04-17 18:01:32 -0700

    registered models

--- ocr_backend/ocr/admin.py
+++ ocr_backend/ocr/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import OCR_file
 
 # Register your models here.
+admin.site.register.(OCR_file)
--- ocr_backend/ocr/models.py
+++ ocr_backend/ocr/models.py
@@ -4,4 +4,8 @@ from django.db import models
 class OCR_file(models.Model):
     title = models.CharField(max_length=200)
     description =  models.TextField()
+
+    def __str__(self):
+        """ A string representation of the model. """
+        return self.title 
    
