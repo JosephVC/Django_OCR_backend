2082943a2b0f18d57b3172c3f47e0a52e40ddca2
Joseph Cardenas <josephvcardenas@gmail.com>
2020-05-18 00:02:29 -0700

    altered models to use FileField rather than ImageField to allow upload pdfs

--- ocr_backend/ocr/models.py
+++ ocr_backend/ocr/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    image = models.ImageField(upload_to='post_images')
+    image = models.FileField(upload_to='post_images')
+    
     
     def __str__(self):
         return self.title
