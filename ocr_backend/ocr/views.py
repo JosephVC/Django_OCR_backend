7d343ba4aef003323ab5982d52df6630757504d7
Joseph Cardenas <josephvcardenas@gmail.com>
2020-05-27 23:14:38 -0700

    re-commit after abort

--- ocr_backend/ocr/views.py
+++ ocr_backend/ocr/views.py
@@ -1,7 +1,10 @@
+from django.http import FileResponse
 from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
+import ocrmypdf
+from subprocess import Popen, PIPE, STDOUT
 
 from .serializers import FileSerializer
 from .models import Post
@@ -15,9 +18,18 @@ class PostViews(APIView):
         return Response(serializer.data)
 
     def post(self, request, *args, **kwargs):
+        
         posts_serializer = FileSerializer(data=request.data)
         if posts_serializer.is_valid():
-            posts_serializer.save()
+            
+            # THE BELOW WORKS GREAT,  NOW TO REMOVE THE HARD-CODED PATH
+            # process = Popen(['ocrmypdf', r"C:\Users\Joseph\Desktop\backpack-scan.pdf", 'output.pdf'])
+            # print(process)
+            # output_file = process.stdout.read()
+          
+            # The below removes the necessity to hard-code the path to the input file.
+            uploaded = posts_serializer.save()
+            process = Popen(['ocrmypdf', uploaded.file.path, 'output.pdf'])
             return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
         else:
             print('error', posts_serializer.errors)
