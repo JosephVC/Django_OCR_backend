ae5acfc8024803f2cc01c87678b1a5ef432ab6bd
JosephVC <josephvcardenas@gmail.com>
2020-06-05 21:10:14 -0700

    used 'poetry run pip freeze' to create more sane requirements.txt

--- django-ocr-backend/django-ocr-backend/settings.py
+++ django-ocr-backend/django-ocr-backend/settings.py
@@ -1,4 +1,3 @@
-import django_heroku
 
 """
 Django settings for django-ocr-backend project.
@@ -11,8 +10,9 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import django_heroku
 import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'whitenoise.runserver_nostatic',  # < As per whitenoise documentation
+    'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
     'ocr',
@@ -55,9 +56,10 @@ REST_FRAMEWORK = {
 }
 
 MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -143,5 +145,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Configure Django App for Heroku.
-import django_heroku
 django_heroku.settings(locals())
