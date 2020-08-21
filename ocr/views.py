a5564d518308c1b4ee4ebfd158f4f1a1b80eeedd
JosephVC <josephvcardenas@gmail.com>
2020-08-20 23:07:50 -0700

    added S3 code to views

--- ocr/views.py
+++ ocr/views.py
@@ -31,9 +31,25 @@ class PostViews(APIView):
         if posts_serializer.is_valid():
                      
             # The below removes the necessity to hard-code the path to the input file.
-            uploaded = posts_serializer.save()
+            uploaded = posts_serializer.save()  
+            
             process = Popen(['ocrmypdf', uploaded.file.path, 'output.pdf'])
+
             return Response(posts_serializer.data, status=status.HTTP_201_CREATED)
+
+            # now that weve uploaded and processed our pdf, 
+            # it's time to send it of to the AWS bucket
+
+            data = open('output.pdf', 'rb')
+
+            s3 = boto3.resource(
+                's3',
+                aws_access_key_id=ACCESS_KEY_ID,
+                aws_secret_access_key=ACCESS_SECRET_KEY,
+                config=Config(signature_version='s3v4')
+            )
+            s3.Bucket(BUCKET_NAME).put_object(Key='media/output.pdf', Body=data)
+
         else:
             print('error', posts_serializer.errors)
             return Response(posts_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
