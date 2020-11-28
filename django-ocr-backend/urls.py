a26ba3fc3b9a91fddd266025e6936f2ecf9cc628
JosephVC <josephvcardenas@gmail.com>
2020-11-27 18:17:58 -0800

    minor updates

--- django-ocr-backend/urls.py
+++ django-ocr-backend/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls'))
+    path('account/', include('allauth.urls'))
 ]
 
 if settings.DEBUG:
