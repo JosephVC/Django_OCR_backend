d1ef612ce2140b80628f35e4acb32bdf765baa94
JosephVC <josephvcardenas@gmail.com>
2021-01-09 18:44:52 -0800

    changing username variable accross files

--- users/serializers.py
+++ users/serializers.py
@@ -4,12 +4,12 @@ from users.models import NewUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(required=True)
-    user_name = serializers.CharField(required=True)
+    username = serializers.CharField(required=True)
     password = serializers.CharField(min_length=8, write_only=True)
 
     class Meta:
         model = NewUser
-        fields = ('email', 'user_name', 'password')
+        fields = ('email', 'username', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
