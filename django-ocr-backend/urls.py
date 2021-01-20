db241ccf0e4b2cde687b95bae902da1d24fb427c
JosephVC <josephvcardenas@gmail.com>
2021-01-19 16:15:38 -0800

    changed the stock 'admin' url for security's sake

--- django-ocr-backend/urls.py
+++ django-ocr-backend/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     #OAuth
     path('auth/', include('drf_social_oauth2.urls', namespace='drf')), 
     # Project URLs
-    path('admin/', admin.site.urls),
+    # For security purposes, the stock '/admin/' url has been changed
+    path('cynosure/', admin.site.urls),
     path('', include('ocr.urls', namespace='ocr')),
     # User Management
     path('api/user/', include('users.urls', namespace='users')),
